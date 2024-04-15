d844bac72733b1d1e0f2af0f3bfef4eb0d86f8a2
Adrian Valera <creativecomplex@gmail.com>
2024-04-14 21:57:44 -0400

    fixed added update-prompt loading

---
+++ app/update-prompt/loading.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+const loading = () => {
+  return (
+    <div>
+        <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+    </div>
+  )
+}
+
+export default loading
